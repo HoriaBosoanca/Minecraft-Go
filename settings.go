4901b7ed248913bbda44448f43ec4427d9061fed
REDplayer333HHH <horia.bosoanca@gmail.com>
2025-04-26 19:25:46 +0300

    render distance

--- settings.go
+++ settings.go
@@ -8,9 +8,9 @@ var fps int32 = 60
 
 var camera3D = rl.Camera{
 	Position:   rl.NewVector3(1.0, 60.0, 1.0),
-	Target:     rl.NewVector3(2.0, 58.0, 2.0),
+	Target:     rl.NewVector3(16.0, 30.0, 16.0),
 	Up:         rl.NewVector3(0.0, 1.0, 0.0),
-	Fovy:       45.0,
+	Fovy:       90.0,
 	Projection: rl.CameraPerspective,
 }
 
